2e5dab87154cfd735c903c3501d58fcdce7ff9ff
Naama Tamir <106441652+emma177@users.noreply.github.com>
2023-07-30 10:59:30 +0300

    register admin

--- DAL/registerAPI.js
+++ DAL/registerAPI.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const registerURL =`${process.env.USERS_BASE_URL}/authUsers/register`;
   // 'http://localhost:8001/authUsers/register';
 
-const register = async ({ username, password, firstName, lastName }) => {
+const register = async ({ username, password, firstName, lastName , isAdmin}) => {
   try {
-    const response = await axios.post(registerURL, { username, password , firstName, lastName});
+    const response = await axios.post(registerURL, { username, password , firstName, lastName, isAdmin});
     return response.data;
   } catch (error) {
     console.error('Failed to register', error);
